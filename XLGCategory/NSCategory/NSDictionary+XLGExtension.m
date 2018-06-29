14616ca2c6f6c90c38cdfb1efb1ced6edb96b84f
hanzhanbing <1655661337@qq.com>
2018-06-29 14:32:49 +0800

    解决NSArray、NSDictionary打印乱码的问题

--- XLGCategory/NSCategory/NSDictionary+XLGExtension.m
+++ XLGCategory/NSCategory/NSDictionary+XLGExtension.m
@@ -69,7 +69,34 @@
     
     [str appendString:@"}\n"];
     
+    str = [NSString stringWithCString:[str cStringUsingEncoding:NSUTF8StringEncoding] encoding:NSNonLossyASCIIStringEncoding];
+    
     return str;
+}
+
+
+- (NSString *)descriptionWithLocale:(id)locale
+{
+    NSMutableString *string = [NSMutableString string];
+    
+    // 开头有个{
+    [string appendString:@"{\n"];
+    
+    // 遍历所有的键值对
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [string appendFormat:@"\t%@", key];
+        [string appendString:@" : "];
+        [string appendFormat:@"%@,\n", obj];
+    }];
+    
+    // 结尾有个}
+    [string appendString:@"}"];
+    
+    // 查找最后一个逗号
+    NSRange range = [string rangeOfString:@"," options:NSBackwardsSearch];
+    if (range.location != NSNotFound)
+        [string deleteCharactersInRange:range];
+    return string;
 }
 
 @end
